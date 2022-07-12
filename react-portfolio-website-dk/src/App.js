5f4a6bc3c1c963a49c86eb5baa1eff0ffe3ab4e8
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2022-07-12 17:30:52 +0100

    pages working

--- react-portfolio-website-dk/src/App.js
+++ react-portfolio-website-dk/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import './index.css'
+import About from './routes/About'
+import Contact from './routes/Contact'
+import Home from './routes/Home'
+import Project from './routes/Project'
+
+import { Route, Routes } from 'react-router-dom'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello You</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
