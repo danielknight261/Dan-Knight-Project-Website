4d8a2d071b5daecaf49e102dccbb7451b22ac723
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2022-07-18 10:49:52 +0100

    Project and Contact buttons compelte

--- react-portfolio-website-dk/src/components/HeroImg.js
+++ react-portfolio-website-dk/src/components/HeroImg.js
@@ -1,6 +1,7 @@
 import "./HeroImg.css"
 import React from 'react'
-import IntroImg from "../assets/intro-bg.jpg"
+import IntroImg from "../assets/background.jpg"
+import { Link } from "react-router-dom";
 
 export const HeroImg = () => {
   return (
@@ -8,10 +9,18 @@ export const HeroImg = () => {
         <div className="mask">
         <img  className="into-img" src={IntroImg} alt="IntroImg" />
         </div>
+        <div  className="content">
+          <p>Hi, I'm a Full-Stack Engineer</p>
+          <h1>React Developer</h1>
+          <div>
+          <Link to="/project" className="button">Projects</Link>
+          <Link to="/contact" className="button-light">Contact</Link>
+          </div>
+        </div>
     </div>
   );
 };
 
 export default HeroImg;
 
-/*hosting image as background in componenets also upto 53 mins. */
+/*hosting image as background in components also upto 53 mins. */
