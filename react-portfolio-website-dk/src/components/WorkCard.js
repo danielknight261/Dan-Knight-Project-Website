26545b63b6fdd171806e2a57ac6acbe8ea3af5d4
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2022-08-19 11:27:02 +0100

    alt tag change

--- react-portfolio-website-dk/src/components/WorkCard.js
+++ react-portfolio-website-dk/src/components/WorkCard.js
@@ -7,7 +7,7 @@ import {NavLink } from "react-router-dom"
 const WorkCard = (props) => {
   return (
     <div className="project-card">
-    <img src={props.imgsrc} alt="image" />
+    <img src={props.imgsrc} alt="project" />
     <h2 className="project-title">{props.title}</h2>
     <div className="pro-details">
         <p>{props.text}</p>
