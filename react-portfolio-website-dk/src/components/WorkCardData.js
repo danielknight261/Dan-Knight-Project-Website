c4e66236f2b1505ce7328adcb86353a0bde27201
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2022-08-23 07:47:41 +0100

    redeem link working

--- react-portfolio-website-dk/src/components/WorkCardData.js
+++ react-portfolio-website-dk/src/components/WorkCardData.js
@@ -1,7 +1,28 @@
 import pro1 from "../assets/project1.jpg"
-import pro2 from "../assets/project.2.JPG"
+import pro2 from "../assets/project2.jpg"
 
 const WorkCardData = [
+    {
+        imgsrc: pro2,
+        title: "Redeem",
+        test: "Team Full Stack Application",
+        view: "http://final-project-front-end-boolean-pirates.vercel.app/",
+        source: "https://github.com/danielknight261/Dan-Knight-Project-Website.git"
+    },
+    {
+        imgsrc: pro1,
+        title: "Eccomerce Website Design",
+        test: "lots of jumbled desciption text in here",
+        view: "https://www.bbc.co.uk/news",
+        source: "https://github.com/danielknight261/Dan-Knight-Project-Website.git"
+    },
+    {
+        imgsrc: pro1,
+        title: "Eccomerce Website Design",
+        test: "lots of jumbled desciption text in here",
+        view: "https://www.bbc.co.uk/news",
+        source: "https://github.com/danielknight261/Dan-Knight-Project-Website.git"
+    },
     {
         imgsrc: pro2,
         title: "Redeem",
